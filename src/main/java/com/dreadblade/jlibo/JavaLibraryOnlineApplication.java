3ae86fdc2321ff66f70a236953372db16408f102
Anton <sqandex1suggestions@gmail.com>
2020-11-15 19:22:25 +0200

    Initial commit

---
+++ src/main/java/com/dreadblade/jlibo/JavaLibraryOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.dreadblade.jlibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLibraryOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLibraryOnlineApplication.class, args);
+	}
+
+}
